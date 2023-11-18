ae2a42294015d9cca2f2059e90a1670215b943d4
Plinio Mabesi <pliniomabesi@gmail.com>
2023-11-18 20:53:52 -0300

    Initial contract functions and event

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -4,10 +4,15 @@ pragma solidity ^0.8.19;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract NFTMarket {
 
+    using Counters for Counters.Counter;
+    Counters.Counter private _itemIds;
+    Counters.Counter private _itemsSold;
+    
     address payable owner;
     uint public listingPrice = 0.025 ether;
 
@@ -27,9 +32,34 @@ contract NFTMarket {
 
     mapping(uint => MarketItem) public marketItems; //item id => market item
 
+    event MarketItemCreated(
+        uint indexed itemId,
+        address indexed nftContract,
+        uint indexed tokenId,
+        address seller,
+        uint price
+    );
+
     function createMarketItem(address nftContract, uint tokenId, uint price) public payable {
         require(price > 0, "Price cannot be zero");
         require(msg.value == listingPrice, "Value must be equal listing price");
+
+        _itemIds.increment();
+        uint itemId = _itemIds.current();
+
+        marketItems[itemId] = MarketItem(
+            itemId,
+            nftContract,
+            tokenId,
+            payable(msg.sender),
+            payable(address(0)),
+            price,
+            false
+        );
+
+        IERC721(nftContract).transferFrom(msg.sender, address(this), tokenId);
+
+        emit MarketItemCreated(itemId, nftContract, tokenId, msg.sender, price);
     }
 
 }
